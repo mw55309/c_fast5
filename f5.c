e712c2ad571d2e15d3752f3a811dcd6c539f3176
mw5309 <mick.watson@roslin.ed.ac.uk>
2015-06-29 19:45:04 +0100

    Updated to extract 2D from all reads given on the command line

--- f5.c
+++ f5.c
@@ -11,74 +11,77 @@ int main(int argc, char *argv[]) {
     size_t      sdim;
     herr_t      status;
     char        *fastq;
- 
-    if ( argc != 2 ) /* argc should be 2 for correct execution */
+    int		i; 
+
+    if ( argc <= 1 ) /* argc should be 2 for correct execution */
     {
-        printf( "usage: %s filename\n", argv[0] );
+        printf( "usage: %s <filename 1> <filename 2> ... <filename n<\n", argv[0] );
 	exit(0);
     }
 
-    /*
-     * Open file
-     */
-    file = H5Fopen (argv[1], H5F_ACC_RDONLY, H5P_DEFAULT);
+    for (i=1; i<argc; i++) {
 
-    /* 
-     * check it exists
-     */
-    status = H5Lexists(file, "/Analyses/Basecall_2D_000/BaseCalled_2D", H5P_DEFAULT);
+	    /*
+	     * Open file
+	     */
+	    file = H5Fopen (argv[1], H5F_ACC_RDONLY, H5P_DEFAULT);
 
-    /*
-     * if it exists, proceed
-     */
-    if (status == 1) {
-	  /*
-           * Open dataset
-           */
-	   dset = H5Dopen (file, DATASET, H5P_DEFAULT);
+	    /* 
+	     * check it exists
+	     */
+	    status = H5Lexists(file, "/Analyses/Basecall_2D_000/BaseCalled_2D", H5P_DEFAULT);
 
-	  /*
-	   * Get the datatype and its size.
-	   */
-	   filetype = H5Dget_type (dset);
-	   sdim = H5Tget_size (filetype);
-	   sdim++;                         /* Make room for null terminator */
+	    /*
+	     * if it exists, proceed
+	     */
+	    if (status == 1) {
+		  /*
+		   * Open dataset
+		   */
+		   dset = H5Dopen (file, DATASET, H5P_DEFAULT);
+
+		  /*
+		   * Get the datatype and its size.
+		   */
+		   filetype = H5Dget_type (dset);
+		   sdim = H5Tget_size (filetype);
+		   sdim++;                         /* Make room for null terminator */
 
 
-	  /*
-	   * allocate memory for the fastq string
-	   */
-	   fastq = (char *) malloc (sdim * sizeof (char *));
+		  /*
+		   * allocate memory for the fastq string
+		   */
+		   fastq = malloc (sdim);
+		  /*
+		   * Create the memory datatype.
+		   */
+		   memtype = H5Tcopy (H5T_C_S1);
+		   status = H5Tset_size (memtype, sdim);
 
-	  /*
-	   * Create the memory datatype.
-	   */
-	   memtype = H5Tcopy (H5T_C_S1);
-	   status = H5Tset_size (memtype, sdim);
+		  /*
+		   * Read the data.
+		   */
+		   status = H5Dread (dset, memtype, H5S_ALL, H5S_ALL, H5P_DEFAULT, fastq);
 
-	  /*
-	   * Read the data.
-	   */
-	   status = H5Dread (dset, memtype, H5S_ALL, H5S_ALL, H5P_DEFAULT, fastq);
+		  /*
+		   * Output the data to the screen.
+		   */
+		   printf("%s", fastq);
 
-	  /*
-	   * Output the data to the screen.
-	   */
-	   printf("%s", fastq);
-
-	   status = H5Dclose (dset);
-           status = H5Tclose (filetype);
-           status = H5Tclose (memtype);
+		   status = H5Dclose (dset);
+		   status = H5Tclose (filetype);
+		   status = H5Tclose (memtype);
 	
-	   free (fastq);
+		   free (fastq);
 
-    } else {
-           // printf ("The dataset either does NOT exist\n or some other error occurred.\n"); 
+	    } else {
+		   // printf ("The dataset either does NOT exist\n or some other error occurred.\n"); 
+	    }
+	
+	    /*
+	     * Close and release resources.
+	     */
+	    status = H5Fclose (file);
     }
-	
-    /*
-     * Close and release resources.
-     */
-    status = H5Fclose (file);
     return 0;
 }
